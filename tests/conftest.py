848b03385423d41e97bac6fb4ef286621cd332a4
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2023-04-06 16:13:35 +0300

    move test_video to capture test file

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,27 +2,21 @@ import cv2
 import pytest
 
 
-# sample video for testing
-@pytest.fixture(scope="module", autouse=True)
-def test_video():
-    return "tests/sample/test.avi"
-
-
 # sample image (frame) for testing
 @pytest.fixture(scope="module", autouse=True)
 def test_frame():
     return cv2.imread("tests/sample/frames/frame1.jpg")
 
 
-# sample metadata for testing
+# casia formatted metadata for testing
 @pytest.fixture(scope="module", autouse=True)
-def test_metadata():
+def test_json():
     return "tests/sample/test.json"
 
 
 # sample frame details for testing
 @pytest.fixture(scope="module", autouse=True)
-def test_frame_details():
+def test_sample():
     return {
         "face_rect": [73, 238, 568, 986],
         "lm7pt": [
@@ -48,6 +42,40 @@ def test_frame_details():
         },
         "feedback": 0,
         "review_status": 1,
+    }
+
+
+@pytest.fixture(scope="module", autouse=True)
+def test_sample_ours():
+    """Our format for metadata"""
+    return {
+        "image": None,
+        "meta": {
+            "face_rect": [73, 238, 568, 986],
+            "face_landmark": [
+                148,
+                616,
+                264,
+                626,
+                450,
+                630,
+                564,
+                616,
+                357,
+                806,
+                232,
+                968,
+                465,
+                976,
+            ],
+            "annot": {
+                "occlusion": "TG",
+                "eye_open_left": 0.9997702836990356,
+                "eye_open_right": 0.9998661875724792,
+            },
+            "feedback": 0,
+            "review_status": 1,
+        },
     }
 
 
