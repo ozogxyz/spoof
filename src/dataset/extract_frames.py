d4a2aeb7e6ef7b3b2cabdf1ac175a36aa0285b05
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2023-04-08 16:17:08 +0300

    add labels.csv and labels to end of frame jgps

--- src/dataset/extract_frames.py
+++ src/dataset/extract_frames.py
@@ -1,19 +1,17 @@
+import csv
 import os
-import sys
 from pathlib import Path
 
 import cv2
-import hydra
-from omegaconf import DictConfig
 
-# Add parent directory to path for easy import
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-from utils import filter_files_by_ext
+def filter_files_by_ext(path: str, ext: str):
+    for path in Path(path).rglob(f"*{ext}"):  # type: ignore
+        yield str(path)
 
 
 # Function to extract frames
-def capture_frames(src: str, dest: str) -> None:
+def capture_frames(src: str, dest: str, label: str) -> None:
     """
     Extracts frames from a video and saves them to a directory.
     """
@@ -24,17 +22,54 @@ def capture_frames(src: str, dest: str) -> None:
             ret, frame = cap.read()
             if not ret:
                 break
-            cv2.imwrite(os.path.join(dest, "frame%d.jpg" % cur_frame), frame)
+            # get src filename without extension
+            frame_name = create_filename(src, label, cur_frame)
+            filename = os.path.join(dest, frame_name)
+            cv2.imwrite(filename, frame)
             cur_frame += 1
         cap.release()
 
-        # print("Finished extracting frames from {}".format(src))
+        print("Finished extracting frames from {}".format(src))
         print("Frames saved to {}".format(dest))
         print("Total frames: {}".format(cur_frame))
 
     else:
         print("Cannot open video file")
     cv2.destroyAllWindows()
+
+
+def create_filename(src, label, cur_frame):
+    video_filename = Path(src).stem
+    frame_name = (
+        video_filename + "_frame_" + str(cur_frame) + "_" + label + ".jpg"
+    )
+
+    return frame_name
+
+
+def create_labels_csv(src: str, dest: str, ext: str = ".jpg") -> None:
+    """
+    Creates a csv file with the following structure:
+
+    frame_name,label
+
+    Args:
+        src (str): path to video files
+        dest (str): path to destination folder
+        ext (str): video file extension
+
+    """
+    # Get list of video files
+    frame_files = filter_files_by_ext(src, ext)
+
+    with open(os.path.join(dest, "labels.csv"), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["File Name", "Label"])
+
+        for file in frame_files:
+            filename = Path(file).name
+            label = Path(file).stem.split("_")[-1]
+            writer.writerow([filename, label])
 
 
 # Function to extract frames from videos
@@ -74,21 +109,4 @@ def extract_frames(src: str, dest: str, ext: str) -> None:
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
 
-        capture_frames(video_file, str(output_directory))
-
-
-@hydra.main(version_base="1.2", config_path="../..", config_name="config")
-def main(cfg: DictConfig) -> None:
-    # Extract train data
-    extract_frames(
-        cfg.dataset.train_src, cfg.dataset.train_dest, cfg.dataset.ext
-    )
-
-    # Extract test data
-    extract_frames(
-        cfg.dataset.test_src, cfg.dataset.test_dest, cfg.dataset.ext
-    )
-
-
-if __name__ == "__main__":
-    main()
+        capture_frames(video_file, str(output_directory), label)
---
+++ tests/test_extract_frames.py
@@ -0,0 +1,27 @@
+import os
+
+import pytest
+
+from src.dataset.extract_frames import extract_frames, create_labels_csv
+
+
+# @pytest.skip(reason="Too long to run")
+# def test_extract_frames():
+#     train_src = (
+#         "/Users/motorbreath/mipt/thesis/datasets/casia/train/data/train"
+#     )
+#     dest = "tests/sample/frames"
+#     ext = ".avi"
+
+#     extract_frames(src=train_src, dest=dest, ext=ext)
+
+#     assert os.path.exists(dest)
+#     assert os.path.exists(os.path.join(dest, "labels.csv"))
+
+
+def test_create_labels_csv():
+    train_src = "/Users/motorbreath/mipt/thesis/code/spoof/tests/sample/frames"
+    dest = "tests/sample/"
+
+    create_labels_csv(src=train_src, dest=dest, ext=".jpg")
+    assert os.path.exists(os.path.join(dest, "labels.csv"))
