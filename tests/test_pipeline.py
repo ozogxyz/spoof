5ac796d1e1b4ef176cc609264268e9a8969bbc06
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2023-04-08 21:41:42 +0300

    add test_pipeline

---
+++ tests/test_pipeline.py
@@ -0,0 +1,9 @@
+import pytest
+from src.dataset.casia import CASIA
+from src.dataset.transforms import FaceRegionRCXT
+from src.dataset.visualize import show_frame
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_pipeline():
+    pass
