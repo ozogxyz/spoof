5f03213ac6fee259a2e2466c8af3a3f4588fd4a4
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2023-04-09 22:38:42 +0300

    add length comparisons

--- tests/test_casia_dataset.py
+++ tests/test_casia_dataset.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import pytest
 
-from src.preprocessing.casia import extract_frames, extract_metadata, CASIA
+from src.preprocessing.casia import Sampler, extract_frames, extract_metadata, CASIA
 from src.preprocessing.transforms import FaceRegionRCXT
 from src.utils.annotations import create_annotations
 from src.utils.visualize import show_frame
@@ -22,9 +22,11 @@ def test_extract_train_frames():
 
     # Check that the number of frames extracted is correct
     count = 0
-    for path in Path(save_dest).rglob("*.jpg"):
+    for _ in Path(save_dest).rglob("*.jpg"):
         count += 1
+    assert count == 45141
     assert fr_n == count
+    assert fr_n == 45141
 
 
 @pytest.mark.skip(reason="Slow")
@@ -54,9 +56,11 @@ def test_extract_train_meta():
 
     # Check that the number of frames extracted is correct
     count = 0
-    for path in Path(save_dest).rglob("*.json"):
+    for _ in Path(save_dest).rglob("*.json"):
         count += 1
+    assert count == 45141
     assert mn == count
+    assert mn == 45141
 
 
 @pytest.mark.skip(reason="Missing folders")
@@ -75,20 +79,25 @@ def test_extract_test_meta():
     assert mn == count
 
 
+# @pytest.mark.skip(reason="Slow")
 def test_casia_dataset():
-    # create_annotations("data/casia/train_frames", "train.txt")
     transform = FaceRegionRCXT(size=(224, 224))
     dataset = CASIA(
         "data/casia/train_frames",
-        annotations_file="data/train.txt",
+        annotations="data/casia/annotations.txt",
+        meta_file="data/casia/meta.txt",
         transform=transform,
-        meta_file="data/meta.txt",
+        sampler=Sampler(),
     )
 
-    # convert tensor to opencv image
-    image = dataset[1241][0].numpy().transpose(1, 2, 0)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-    show_frame(title="Sample input to CNN", frame=image)
     assert len(dataset) == 45141
-    assert image.shape == (224, 224, 3)
+    assert len(open("data/casia/annotations.txt").readlines()) == 45141
+    assert len(open("data/casia/meta.txt").readlines()) == 45141
+
+    # convert tensor to opencv image
+    # image = dataset[1241][0].numpy().transpose(1, 2, 0)
+    # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # show_frame(title="Sample input to CNN", frame=image)
+    # assert len(dataset) == 45141
+    # assert image.shape == (224, 224, 3)
