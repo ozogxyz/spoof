1ac041692977a47503aba15fe485432ff5ca4339
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2023-04-09 16:05:03 +0300

    add absolute paths

--- src/utils/annotations.py
+++ src/utils/annotations.py
@@ -1,24 +1,45 @@
 import os
+from pathlib import Path
 
 
 def create_annotations(data_root, train_file):
-    """Generate the train file, which has the following format.
+    """Generate the train file, which has the following format. torch.Dataset class will need the
+    absolute path while reading from cv.imread(absolute_path).
 
-    relative_path0 label0 relative_path1 label1 relative_path2 label2 ...
+    absolute_path0 label0 absolute_path1 label1 ...
     """
     train_file_buf = open(train_file, "w")
-    id_list = os.listdir(data_root)
-    id_list.sort()
-    for label, id_name in enumerate(id_list):
-        cur_id_folder = os.path.join(data_root, id_name)
-        cur_img_list = os.listdir(cur_id_folder)
-        cur_img_list.sort()
-        for _, image_name in enumerate(cur_img_list):
-            image_path = os.path.join(id_name, image_name)
-            train_file_buf.write(image_path + " " + str(label) + "\n")
+    id_list = os.walk(data_root)
+    for root, _, files in id_list:
+        files.sort()
+        for file in files:
+            file = str(Path(root) / Path(file))
+            label = Path(file).stem[-1]
+            if Path(file).suffix == ".jpg":
+                train_file_buf.write(file + " " + label + "\n")
+
+
+def create_meta(data_root, meta_file):
+    """Generate the meta file, which has the following format.
+
+    absolute_path0 label0 absolute_path1 label1 ...
+    """
+    meta_file_buf = open(meta_file, "w")
+    id_list = os.walk(data_root)
+    for root, _, files in id_list:
+        files.sort()
+        for file in files:
+            file = str(Path(root) / Path(file))
+            label = Path(file).stem[-1]
+            if Path(file).suffix == ".json":
+                meta_file_buf.write(file + " " + label + "\n")
 
 
 if __name__ == "__main__":
-    data_root = "/Users/motorbreath/mipt/thesis/datasets/casia/train/data/train"
+    data_root = (
+        "/Users/motorbreath/mipt/thesis/code/spoof/data/casia/train_frames"
+    )
     train_file = "/Users/motorbreath/mipt/thesis/code/spoof/data/train.txt"
+    meta_file = "/Users/motorbreath/mipt/thesis/code/spoof/data/meta.txt"
     create_annotations(data_root, train_file)
+    create_meta(data_root, meta_file)
