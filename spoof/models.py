ee1c3ff7ebdd73c84d615c41703ffda1af7348bc
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2023-04-13 14:51:14 +0300

    timm draft

--- spoof/models.py
+++ spoof/models.py
@@ -1,96 +1,23 @@
-import torch
-from datasets import CASIA
-from torchvision import transforms
-from transforms import FaceRegionRCXT, MetaAddLMSquare
-from vit_pytorch import ViT
 import torch.nn as nn
-from torch import optim
+from timm.models.vision_transformer import VisionTransformer
 
 
-def _test_vit():
-    model = ViT(
-        image_size=224,
-        patch_size=16,
-        num_classes=2,
-        dim=1024,
-        depth=6,
-        heads=8,
-        mlp_dim=2048,
-        dropout=0.1,
-        emb_dropout=0.1,
-    )
-    ds = CASIA(
-        annotations_path="data/casia/test_annotations.json",
-        video_root="data/casia/test",
-        img_root="data/casia/images/test",
-        extract=False,
-        transform=transforms.Compose(
-            [MetaAddLMSquare(), FaceRegionRCXT(size=(224, 224))]
-        ),
-    )
-    model = model.to("mps")
+class ViT(nn.Module):
+    def __init__(
+        self,
+        model: VisionTransformer,
+        n_classes: int,
+    ):
+        """
+        Args:
+            `model`: The transformer model.
+            `n_classes`: Number of classes in the classification task.
+        """
+        super().__init__()
+        self.model = model
+        self.classifier = nn.Linear(model.num_features, n_classes)
 
-    # Split the train dataset into train and validation
-    train_size = int(0.8 * len(ds))
-    valid_size = len(ds) - train_size
-
-    train_dataset, valid_dataset = torch.utils.data.random_split(
-        ds, [train_size, valid_size]
-    )
-
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=32, shuffle=True, num_workers=4
-    )
-    valid_loader = torch.utils.data.DataLoader(
-        valid_dataset, batch_size=32, shuffle=False, num_workers=4
-    )
-
-    criterion = nn.CrossEntropyLoss()
-
-    optimizer = optim.Adam(model.parameters(), lr=1e-4)
-    device = "mps"
-    for epoch in range(1):
-        epoch_loss = 0
-        epoch_accuracy = 0
-
-        for data, label in train_loader:
-            data = data["image"]
-            data = data.permute(0, 3, 1, 2)  # NCHW
-
-            data = data.to(device)
-            label = label.to(device)
-
-            output = model(data)
-            loss = criterion(output, label)
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            acc = (output.argmax(dim=1) == label).float().mean()
-            epoch_accuracy += acc / len(train_loader)
-            epoch_loss += loss / len(train_loader)
-
-        with torch.no_grad():
-            epoch_val_accuracy = 0
-            epoch_val_loss = 0
-            for data, label in valid_loader:
-                data = data["image"]
-                data = data.permute(0, 3, 1, 2)  # NCHW
-                data = data.to(device)
-                label = label.to(device)
-
-                val_output = model(data)
-                val_loss = criterion(val_output, label)
-
-                acc = (val_output.argmax(dim=1) == label).float().mean()
-                epoch_val_accuracy += acc / len(valid_loader)
-                epoch_val_loss += val_loss / len(valid_loader)
-
-        print(
-            f"Epoch : {epoch+1} - loss : {epoch_loss:.4f} - acc: {epoch_accuracy:.4f} - val_loss : {epoch_val_loss:.4f} - val_acc: {epoch_val_accuracy:.4f}\n"
-        )
-
-
-if __name__ == "__main__":
-    _test_vit()
+    def forward(self, x):
+        x = self.model(x)
+        x = self.classifier(x)
+        return x
--- spoof/train.py
+++ spoof/train.py
@@ -1,96 +1,57 @@
-# import copy
-# import time
-
-# import hydra
-# import torch
-# from omegaconf import DictConfig
-
-# from torchvision.transforms import Compose
-# from torch import optim
-# from datasets import CASIA
-# from models import ViT
-# from transforms import FaceRegionRCXT, MetaAddLMSquare
+import hydra
+import timm
+import torchsummary
+from omegaconf import DictConfig, OmegaConf
+from torch.utils.data import DataLoader
+from datasets import CASIA
+import torch
 
 
-# def train_model(model, dataloader, criterion, optimizer, num_epochs=25):
-#     device = "mps"
-#     since = time.time()
+@hydra.main(version_base=None, config_path="..", config_name="config")
+def main(cfg: DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg.model))
 
-#     best_model_wts = copy.deepcopy(model.state_dict())
-#     best_acc = 0.0
+    model = timm.create_model(cfg.model.name, **cfg.model.params)
 
-#     for epoch in range(num_epochs):
-#         print(f"Epoch {epoch}/{num_epochs - 1}")
-#         print("-" * 10)
+    torchsummary.summary(model, (3, 224, 224), device="cuda")
 
-#         model.train()
+    # Casia dataset and dataloader
+    casia = CASIA(**cfg.dataset.params)
 
-#         running_loss = 0.0
-#         running_corrects = 0
+    # Split the dataset into train and validation
+    train_size = int(0.8 * len(casia))
+    val_size = len(casia) - train_size
+    train_dataset, val_dataset = torch.utils.data.random_split(
+        casia, [train_size, val_size]
+    )
 
-#         for samples, labels in dataloader:
-#             img = samples["image"]
-#             print(img)
-#             break
-#             image = image.to(device)
-#             labels = labels.to(device)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=cfg.train.batch_size,
+        shuffle=True,
+        num_workers=cfg.train.num_workers,
+    )
 
-#             # zero the parameter gradients
-#             optimizer.zero_grad()
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=cfg.train.batch_size,
+        shuffle=False,
+        num_workers=cfg.train.num_workers,
+    )
 
-#             # forward
-#             outputs = model(image)
-#             _, preds = torch.max(outputs, 1)
-#             loss = criterion(outputs, labels)
-#             loss.backward()
-#             optimizer.step()
+    # Train the model
+    for epoch in range(cfg.train.epochs):
+        for i, (samples, labels) in enumerate(train_loader):
+            images = samples["image"]
+            print(
+                f"Epoch: {epoch}, Batch: {i}, Images: {images.shape}, Labels: {labels.shape}"
+            )
 
-#             # statistics
-#             running_loss += loss.item() * image.size(0)
-#             running_corrects += torch.sum(preds == labels.data)
-
-#         epoch_loss = running_loss / len(dataloader.dataset)
-#         epoch_acc = running_corrects.double() / len(dataloader.dataset)
-
-#         print(f"Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
-
-#         # deep copy the model
-#         if epoch_acc > best_acc:
-#             best_acc = epoch_acc
-#             best_model_wts = copy.deepcopy(model.state_dict())
-
-#         print()
-
-#     time_elapsed = time.time() - since
-#     print(f"Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s")
-#     print(f"Best val Acc: {best_acc:4f}")
-
-#     # load best model weights
-#     model.load_state_dict(best_model_wts)
-#     return model
+        for i, (images, labels) in enumerate(val_loader):
+            print(
+                f"Epoch: {epoch}, Batch: {i}, Images: {images.shape}, Labels: {labels.shape}"
+            )
 
 
-# @hydra.main(version_base=None, config_path="..", config_name="config")
-# def main(cfg: DictConfig) -> None:
-#     casia = CASIA(
-#         annotations_path=cfg.dataset.annotations_path,
-#         video_root=cfg.dataset.video_root,
-#         img_root=cfg.dataset.img_root,
-#         extract=False,
-#         transform=Compose([MetaAddLMSquare(), FaceRegionRCXT(size=(224, 224))]),
-#     )
-#     train_loader = torch.utils.data.DataLoader(
-#         casia,
-#         batch_size=cfg.train.batch_size,
-#         shuffle=True,
-#         num_workers=cfg.train.num_workers,
-#     )
-
-#     model = ViT(num_classes=2).to("mps")
-#     criterion = torch.nn.BCELoss()
-#     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-#     model = train_model(model, train_loader, criterion, optimizer, num_epochs=25)
-
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
