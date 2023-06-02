d9f0ca61133b9707fd8c5414d5317c171b0786a6
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2023-06-03 00:01:58 +0300

    make val ds single value, not list_ds_val

--- src/spoof/training_system.py
+++ src/spoof/training_system.py
@@ -302,11 +302,14 @@ class SpoofClassificationSystem(SpoofClassificationValidator):
             self.ds_train = instantiate(data_config["train"])
             self.ds_train.name = "train"
 
-            val_subsets = [k for k in data_config.keys() if "val" in k]
-            self.list_ds_val = []
-            for name in val_subsets:
-                self.list_ds_val.append(instantiate(data_config[name]))
-                self.list_ds_val[-1].name = name
+            # val_subsets = [k for k in data_config.keys() if "val" in k]
+            # self.list_ds_val = []
+            # for name in val_subsets:
+            #     self.list_ds_val.append(instantiate(data_config[name]))
+            #     self.list_ds_val[-1].name = name
+
+            self.ds_val = instantiate(data_config["val_base"])
+            self.ds_val.name = "val_base"
 
     def train_dataloader(self) -> torch.utils.data.DataLoader:
         """
@@ -330,15 +333,21 @@ class SpoofClassificationSystem(SpoofClassificationValidator):
         """
         is_cuda = "cuda" in self.device.type
         num_workers = 0 if os.name == "nt" else 4
-        loaders_val = [
-            DataLoader(
-                elem,
-                batch_size=self.eval_batch_size,
-                pin_memory=is_cuda,
-                num_workers=num_workers,
-            )
-            for elem in self.list_ds_val
-        ]
+        # loaders_val = [
+        #     DataLoader(
+        #         elem,
+        #         batch_size=self.eval_batch_size,
+        #         pin_memory=is_cuda,
+        #         num_workers=num_workers,
+        #     )
+        #     for elem in self.list_ds_val
+        # ]
+        loaders_val = DataLoader(
+            self.ds_val,
+            batch_size=self.eval_batch_size,
+            pin_memory=is_cuda,
+            num_workers=num_workers,
+        )
         return loaders_val
 
     def on_train_epoch_start(self):
